90d8669372c807452338a85b5d355817abf81367
D.C. Pagan <dcpagan@posteo.net>
2024-06-28 23:42:26 -0400

    Day 10, part 2

--- src/Days/Day10.hs
+++ src/Days/Day10.hs
@@ -3,22 +3,25 @@
 {-# LANGUAGE TypeFamilies #-}
 module Days.Day10 (runDay) where
 
-import Control.Lens
+import Control.Lens hiding (uncons)
 import Data.Array.IArray
-import Data.Bifunctor
-import Data.Functor
-import Data.Functor.Adjunction
 import Data.Distributive
 import Data.Fix
+import Data.Function
+import Data.Functor
+import Data.Functor.Contravariant
+import Data.Functor.Adjunction
 import Data.Functor.Rep
 import Data.List
 import Data.Maybe
+import Data.Monoid
 import qualified Util.Util as U
 
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text (Parser, takeText)
 import qualified Data.Text as T
 import Control.Monad.Representable.Reader (distributeRep, collectRep)
+import Control.Arrow
 
 ------------ TYPES ------------
 data MazeTile where
@@ -85,24 +88,6 @@ instance Representable MazeF where
 instance Distributive MazeF where
   distribute = distributeRep
   collect = collectRep
-
-data StreamF a b where
-  StreamF :: {
-    _a :: a,
-    _b :: b
-  } -> StreamF a b
-  deriving (Eq, Show, Functor)
-makeLenses ''StreamF
-
-instance Bifunctor StreamF where
-  first f StreamF { _a, _b } = StreamF { _a = f _a, ..}
-  second f StreamF { _a, _b } = StreamF { _b = f _b, ..}
-  bimap f g StreamF { _a, _b } = StreamF {
-    _a = f _a,
-    _b = g _b
-  }
-
-type Stream a = Fix (StreamF a)
 
 type Input = (Maze, Step)
 
@@ -230,5 +215,95 @@ partA :: Input -> OutputA
 partA = flip div 2 . length . uncurry mazePath
 
 ------------ PART B ------------
+areHorizontallyContinuousTiles :: MazeTile -> MazeTile -> Bool
+areHorizontallyContinuousTiles a b =
+  turn a West /= Stop && turn b East /= Stop
+
+isHorizontallyContinuous :: CoordF MazeTile -> CoordF MazeTile -> Bool
+isHorizontallyContinuous a b =
+  _y a == _y b && if
+    | _x a == pred (_x b) ->
+      areHorizontallyContinuousTiles (_z a) (_z b)
+    | _x b == pred (_x a) ->
+      areHorizontallyContinuousTiles (_z b) (_z a)
+    | otherwise -> False
+
+isVerticallyContinuous :: CoordF MazeTile -> CoordF MazeTile -> Bool
+isVerticallyContinuous a b =
+  _x a == _x b && if
+    | _y a == pred (_y b) ->
+      turn (_z a) North /= Stop && turn (_z b) South /= Stop
+    | _y b == pred (_y a) ->
+      turn (_z b) North /= Stop && turn (_z a) South /= Stop
+    | otherwise -> False
+
+isContinuous :: CoordF MazeTile -> CoordF MazeTile -> Bool
+isContinuous a b =
+  isHorizontallyContinuous a b && isVerticallyContinuous a b
+
+partitionLatitudes :: [CoordF a] -> [[CoordF a]]
+partitionLatitudes =
+  groupBy (on (==) _y) . sortBy (getComparison (
+    contramap _y defaultComparison <> contramap _x defaultComparison))
+
+groupLatitude :: [CoordF MazeTile] -> [[CoordF MazeTile]]
+groupLatitude = foldr (\a as -> case Data.List.uncons as of
+  Nothing -> [[a]]
+  Just (b, bs) -> if isHorizontallyContinuous a (head b)
+    then (a:b):bs
+    else [a]:as) []
+
+isTangent :: [CoordF MazeTile] -> Bool
+isTangent [] = False
+isTangent [_] = False
+isTangent l = turn (_z . head $ l) West == turn (_z . last $ l) East
+
+-- List hylomorphism
+hylo' :: (Maybe (c, b) -> b) -> (a -> Maybe (c, a)) -> a -> b
+hylo' alg coalg = go
+  where
+    go = alg . (fmap . second) go . coalg
+
+-- List paramorphism
+para' :: (Maybe (a, ([a], b)) -> b) -> [a] -> b
+para' alg = go
+  where
+    go = alg . (fmap . second) (id &&& go) . uncons
+
+-- Take one extra
+takeWhile1 :: (a -> Bool) -> [a] -> [a]
+takeWhile1 p = foldr (\x xs -> if p x then x:xs else [x]) []
+
+-- Drop one extra
+dropWhile1 :: (a -> Bool) -> [a] -> [a]
+dropWhile1 p = para' $ \case
+  Nothing -> []
+  Just (x, (xs, ys)) -> if p x then ys else xs
+
+takeDropWhile1 :: (a -> Bool) -> [a] -> ([a], [a])
+takeDropWhile1 p = takeWhile1 p &&& dropWhile1 p
+
+latitudeArea :: [[CoordF MazeTile]] -> Int
+latitudeArea = hylo' alg coalg
+  where
+    alg :: Maybe ([[CoordF MazeTile]], Int) -> Int
+    alg = \case
+      Nothing -> 0
+      Just (segments, totalLength) -> gapLengths segments + totalLength
+    gapLengths = sum <<<
+      zipWith (\a b -> (_x . head $ b) - (_x . last $ a) - 1) <*> tail
+    coalg :: [[CoordF MazeTile]] -> Maybe ([[CoordF MazeTile]], [[CoordF MazeTile]])
+    coalg [] = Nothing
+    coalg (a:as) = if isTangent a
+      then coalg as
+      else Just $ first (a:) $ takeDropWhile1 isTangent as
+
+coordWithTile :: Maze -> CoordF a -> CoordF MazeTile
+coordWithTile maze = ($>) <*> indexAdjunction maze
+
+pathWithTiles :: Maze -> Step -> [CoordF MazeTile]
+pathWithTiles maze = fmap (coordWithTile maze) . mazePath maze
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = auf (_Wrapping Sum) (foldMapOf traverse) (latitudeArea . groupLatitude)
+  . partitionLatitudes . uncurry pathWithTiles
