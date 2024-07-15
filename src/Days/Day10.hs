bffefb2b65248a8c743a482afb446b40bf528867
D.C. Pagan <dcpagan@posteo.net>
2024-07-14 20:03:35 -0400

    Cleaning

--- src/Days/Day10.hs
+++ src/Days/Day10.hs
@@ -52,63 +52,36 @@ data Direction where
   deriving (Bounded, Enum, Eq, Show)
 makePrisms ''Direction
 
-type I = Word
-
--- Dependent sum
-data CoordF a where
+data CoordF (len :: Nat) (wid :: Nat) a where
   CoordF :: {
-    _len :: I,
-    _wid :: I,
-    _x :: I,
-    _y :: I,
+    _x :: Word,
+    _y :: Word,
     _z :: a
-  } -> CoordF a
+  } -> CoordF (len :: Nat) (wid :: Nat) a
   deriving (Eq, Show, Functor)
 makeLenses ''CoordF
 
-data CoordF' (len :: Nat) (wid :: Nat) a where
-  CoordF' :: {
-    _x' :: Word,
-    _y' :: Word,
-    _z' :: a
-  } -> CoordF' (len :: Nat) (wid :: Nat) a
-  deriving (Eq, Show, Functor)
-makeLenses ''CoordF'
+type Coord len wid = CoordF len wid ()
+type Step len wid = CoordF len wid Direction
 
-type Coord = CoordF ()
-type Step = CoordF Direction
-
-type Coord' len wid = CoordF' len wid ()
-type Step' len wid = CoordF' len wid Direction
-
--- Dependent product
-newtype MazeF a = MazeF { unMazeF :: I -> I -> Array (I, I) a }
+newtype MazeF (len :: Nat) (wid :: Nat) a =
+  MazeF { unMazeF :: Array (Word, Word) a }
   deriving (Functor)
 makeLenses ''MazeF
 
-instance Show (MazeF a) where
-  show _ = "maze"
-
-newtype MazeF' (len :: Nat) (wid :: Nat) a =
-  MazeF' { unMazeF' :: Array (Word, Word) a }
-  deriving (Functor)
-makeLenses ''MazeF'
-
-instance (KnownNat len, KnownNat wid) => Show (MazeF' len wid a) where
+instance (KnownNat len, KnownNat wid) => Show (MazeF len wid a) where
   show _ = "maze"
 
 data MazeStart a b where
   MazeStart :: forall len wid a b. (KnownNat len, KnownNat wid) => {
-    maze :: MazeF' len wid a,
-    start :: CoordF' len wid b
+    maze :: MazeF len wid a,
+    start :: CoordF len wid b
   } -> MazeStart a b
 
 instance Show (MazeStart a b) where
   show _ = "maze start"
 
-type Maze = MazeF MazeTile
-
-type Maze' len wid = MazeF' len wid MazeTile
+type Maze len wid = MazeF len wid MazeTile
 type MazeStart' = MazeStart MazeTile Direction
 
 knownNatToWord :: forall n. (KnownNat n) => Word
@@ -117,49 +90,29 @@ knownNatToWord = naturalToWord $ fromSNat @n natSing
 knownNatToWord' :: forall n. (KnownNat n) => Word
 knownNatToWord' = naturalToWord $ fromSNat @n natSing - 1
 
-instance Adjunction CoordF MazeF where
+instance (KnownNat len, KnownNat wid) =>
+  Adjunction (CoordF len wid) (MazeF len wid) where
   unit _z = MazeF $
-    \_len _wid -> genArray ((1, 1), (_len, _wid)) $
-    \(_x, _y) -> CoordF {..}
-  counit CoordF {..} = unMazeF _z _len _wid ! (_x, _y)
-  leftAdjunct f _z = MazeF $
-    \_len _wid -> genArray ((1, 1), (_len, _wid)) $
-    \(_x, _y) -> f CoordF {..}
-  rightAdjunct f CoordF {..} = unMazeF (f _z) _len _wid ! (_x, _y)
-
-instance Representable MazeF where
-  type Rep MazeF = Coord
-  tabulate = tabulateAdjunction
-  index = indexAdjunction
-
-instance Distributive MazeF where
-  distribute = distributeRep
-  collect = collectRep
-
-instance forall len wid. (KnownNat len, KnownNat wid) =>
-  Adjunction (CoordF' len wid) (MazeF' len wid) where
-  unit _z' = MazeF' $
     genArray ((0, 0), (knownNatToWord' @len, knownNatToWord' @wid)) $
-    \(_x', _y') -> CoordF' {..}
-  counit CoordF' {..} = unMazeF' @len @wid _z' ! (_x', _y')
-  leftAdjunct f _z' = MazeF' $
+    \(_x, _y) -> CoordF {..}
+  counit CoordF {..} = unMazeF _z ! (_x, _y)
+  leftAdjunct f _z = MazeF $
     genArray ((0, 0), (knownNatToWord' @len, knownNatToWord' @len)) $
-    \(_x', _y') -> f CoordF' {..}
-  rightAdjunct f CoordF' {..} = unMazeF' @len @wid (f _z') ! (_x', _y')
+    \(_x, _y) -> f CoordF {..}
+  rightAdjunct f CoordF {..} = unMazeF (f _z) ! (_x, _y)
 
-instance forall len wid. (KnownNat len, KnownNat wid) =>
-  Representable (MazeF' len wid) where
-  type Rep (MazeF' len wid) = CoordF' len wid ()
+instance (KnownNat len, KnownNat wid) =>
+  Representable (MazeF len wid) where
+  type Rep (MazeF len wid) = CoordF len wid ()
   tabulate = tabulateAdjunction
   index = indexAdjunction
 
-instance forall len wid. (KnownNat len, KnownNat wid) =>
-  Distributive (MazeF' len wid) where
+instance (KnownNat len, KnownNat wid) =>
+  Distributive (MazeF len wid) where
   distribute = distributeRep
   collect = collectRep
 
-type Input = (Maze, Step)
-type Input' = (Natural, Natural, MazeStart')
+type Input = MazeStart'
 
 type OutputA = Word
 
@@ -201,16 +154,17 @@ turn SW North = West
 turn SW East = South
 turn _ _ = Stop
 
-nextStep :: Step -> Step
+nextStep :: forall len wid. (KnownNat len, KnownNat wid) =>
+  Step len wid -> Step len wid
 nextStep coord@CoordF {..}
   | _z == Stop = coord
   | _z == North = if _y == 0
     then stop
     else coord { _y = pred _y }
-  | _z == East = if _x >= _wid - 1
+  | _z == East = if _x >= wid
     then stop
     else coord { _x = succ _x }
-  | _z == South = if _y >= _len - 1
+  | _z == South = if _y >= len
     then stop
     else coord { _y = succ _y }
   | _z == West = if _x == 0
@@ -218,168 +172,97 @@ nextStep coord@CoordF {..}
     else coord { _x = pred _x }
   where
     stop = coord { _z = Stop }
-
-nextStep' :: forall len wid. (KnownNat len, KnownNat wid) =>
-  Step' len wid -> Step' len wid
-nextStep' coord@CoordF' {..}
-  | _z' == Stop = coord
-  | _z' == North = if _y' == 0
-    then stop
-    else coord { _y' = pred _y' }
-  | _z' == East = if _x' >= wid
-    then stop
-    else coord { _x' = succ _x' }
-  | _z' == South = if _y' >= len
-    then stop
-    else coord { _y' = succ _y' }
-  | _z' == West = if _x' == 0
-    then stop
-    else coord { _x' = pred _x' }
-  where
-    stop = coord { _z' = Stop }
     len = knownNatToWord' @len
     wid = knownNatToWord' @wid
 
-peek :: Maze -> Step -> MazeTile
-peek maze coord@CoordF { _z = Stop } = zapWithAdjunction const maze coord
-peek maze coord@CoordF { _z = North, _y }
-  | _y == 0 = Ground
-  | otherwise = zapWithAdjunction const maze coord { _y = pred _y }
-peek maze coord@CoordF { _z = East, _x, _wid }
-  | _x >= _wid - 1 = Ground
-  | otherwise = zapWithAdjunction const maze coord { _x = succ _x }
-peek maze coord@CoordF { _z = South, _y, _len }
-  | _y >= _len - 1 = Ground
-  | otherwise = zapWithAdjunction const maze coord { _y = succ _y }
-peek maze coord@CoordF { _z = West, _x }
-  | _x == 0 = Ground
-  | otherwise = zapWithAdjunction const maze coord { _x = pred _x }
+peek :: forall len wid. (KnownNat len, KnownNat wid) =>
+  Maze len wid -> Step len wid -> MazeTile
+peek maze coord@CoordF {..}
+  | _z == Stop = zap coord
+  | _z == North = if _y == 0
+    then Ground
+    else zap coord { _y = pred _y }
+  | _z == East = if _x >= wid
+    then Ground
+    else zap coord { _x = succ _x }
+  | _z == South = if _y >= len
+    then Ground
+    else zap coord { _y = succ _y }
+  | _z == West = if _x == 0
+    then Ground
+    else zap coord { _x = pred _x }
+  where
+    zap = zapWithAdjunction const maze
+    len = knownNatToWord' @len
+    wid = knownNatToWord' @wid
 
-peek' :: forall len wid. (KnownNat len, KnownNat wid) =>
-  Maze' len wid -> Step' len wid -> MazeTile
-peek' maze coord@CoordF' { _z' = Stop } = zapWithAdjunction const maze coord
-peek' maze coord@CoordF' { _z' = North, _y' }
-  | _y' == 0 = Ground
-  | otherwise = zapWithAdjunction const maze coord { _y' = pred _y' }
-peek' maze coord@CoordF' { _z' = East, _x' }
-  | _x' >= knownNatToWord' @wid = Ground
-  | otherwise = zapWithAdjunction const maze coord { _x' = succ _x' }
-peek' maze coord@CoordF' { _z' = South, _y' }
-  | _y' >= knownNatToWord' @len = Ground
-  | otherwise = zapWithAdjunction const maze coord { _y' = succ _y' }
-peek' maze coord@CoordF' { _z' = West, _x' }
-  | _x' == 0 = Ground
-  | otherwise = zapWithAdjunction const maze coord { _x' = pred _x' }
+findStart :: MonadFail m => T.Text -> m Word
+findStart = maybe (fail "Could not find start.") (return . fromIntegral)
+  . T.findIndex (== 'S')
 
-findStart :: MonadFail m => T.Text -> m I
-findStart text = maybe (fail "Could not find start.") (return . fromIntegral)
-  $ T.findIndex (== 'S') text
+findWidth :: MonadFail m => T.Text -> m Word
+findWidth = maybe (fail "Could not find newline.") (return . fromIntegral)
+  . T.findIndex (== '\n')
 
-findWidth :: MonadFail m => T.Text -> m I
-findWidth text = maybe (fail "Could not find newline.") (return . fromIntegral)
-  $ T.findIndex (== '\n') text
-
-findAtCoord :: T.Text -> CoordF a -> MazeTile
+findAtCoord :: forall len wid a. (KnownNat len, KnownNat wid) =>
+  T.Text -> CoordF len wid a -> MazeTile
 findAtCoord text CoordF {..} = toTile $ T.index text $ fromIntegral
-  $ succ _wid * _y + _x
+  $ succ (knownNatToWord @wid) * _y + _x
 
-findAtCoord' :: forall len wid a. (KnownNat len, KnownNat wid) =>
-  T.Text -> CoordF' len wid a -> MazeTile
-findAtCoord' text CoordF' {..} = toTile $ T.index text $ fromIntegral
-  $ succ (knownNatToWord @wid) * _y' + _x'
-
-findFirstStep :: Maze -> Step -> Direction
+findFirstStep :: (KnownNat len, KnownNat wid) =>
+  Maze len wid -> Step len wid -> Direction
 findFirstStep maze coord = fromMaybe Stop . find (/= Stop)
   . fmap (peek maze . (coord $>) >>= turn) $ enumFrom minBound
 
-findFirstStep' :: (KnownNat len, KnownNat wid) =>
-  Maze' len wid -> Step' len wid -> Direction
-findFirstStep' maze coord = fromMaybe Stop . find (/= Stop)
-  . fmap (peek' maze . (coord $>) >>= turn) $ enumFrom minBound
-
-parseMaze :: Parser (Maze, Step)
+parseMaze :: Parser MazeStart'
 parseMaze = do
-  text <- takeText
-  i <- findStart text
-  _wid <- findWidth text
-  let
-    _len = div (fromIntegral $ T.length text) (_wid + 1)
-    _x = rem i (_wid + 1)
-    _y = div i (_wid + 1)
-    _z = findFirstStep maze start
-    maze = tabulate $ findAtCoord text
-    start = CoordF {..}
-  return (maze, start)
-
-parseMaze' :: Parser Input'
-parseMaze' = do
   text <- takeText
   i <- findStart text
   w <- findWidth text
   let
     l = div (fromIntegral $ T.length text) (w + 1)
-    _x' = rem i (w + 1)
-    _y' = div i (w + 1)
+    (_y, _x) = divMod i (w + 1)
     len = naturalFromWord l
     wid = naturalFromWord w
-  withSomeSNat len $ \case
-    SNat @len -> withSomeSNat wid $ \case
-      SNat @wid ->
-        let
-          _z' = findFirstStep' @len @wid maze start
-          maze = tabulate $ findAtCoord' @len @wid text
-          start = CoordF' {..}
-        in
-          return (len, wid, MazeStart maze start)
+  withSomeSNat len $ \(SNat @len) ->
+    withSomeSNat wid $ \(SNat @wid) ->
+      let
+        _z = findFirstStep maze start
+        maze = tabulate $ findAtCoord text
+        start = CoordF {..}
+      in
+        return $ MazeStart @len @wid maze start
 
 inputParser :: Parser Input
 inputParser = parseMaze
 
-inputParser' :: Parser Input'
-inputParser' = parseMaze'
-
 runDay :: R.Day
-runDay = R.runDay parseMaze' partA' partB'
+runDay = R.runDay parseMaze partA partB
 
 ------------ PART A ------------
-stepAndTurn :: Maze -> Step -> Step
+stepAndTurn :: (KnownNat len, KnownNat wid) =>
+  Maze len wid -> Step len wid -> Step len wid
 stepAndTurn maze step = n $> zapWithAdjunction turn maze n
   where
     n = nextStep step
 
-stepAndTurn' :: (KnownNat len, KnownNat wid) =>
-  Maze' len wid -> Step' len wid -> Step' len wid
-stepAndTurn' maze step = n $> zapWithAdjunction turn maze n
-  where
-    n = nextStep' step
-
-mazePath :: Maze -> Step -> [Step]
+mazePath :: (KnownNat len, KnownNat wid) =>
+  Maze len wid -> Step len wid -> [Step len wid]
 mazePath maze = unfoldr $ \step@CoordF{..} -> if _z == Stop
   then Nothing
   else Just (step, stepAndTurn maze step)
 
-mazePath' :: (KnownNat len, KnownNat wid) =>
-  Maze' len wid -> Step' len wid -> [Step' len wid]
-mazePath' maze = unfoldr $ \step@CoordF'{..} -> if _z' == Stop
-  then Nothing
-  else Just (step, stepAndTurn' maze step)
-
 partA :: Input -> OutputA
-partA = flip div 2 . fromIntegral . length . uncurry mazePath
-
-partA' :: Input' -> OutputA
-partA' (len, wid, mazeStart) = withSomeSNat len $ \case
-  SNat @len -> withSomeSNat wid $ \case
-    SNat @wid -> case mazeStart of
-      MazeStart maze start ->
-        flip div 2 $ fromIntegral $ length $ mazePath' maze start
+partA MazeStart { maze, start } =
+  flip div 2 $ fromIntegral $ length $ mazePath maze start
 
 ------------ PART B ------------
 areHorizontallyContinuousTiles :: MazeTile -> MazeTile -> Bool
 areHorizontallyContinuousTiles a b =
   turn a West /= Stop && turn b East /= Stop
 
-isHorizontallyContinuous :: CoordF MazeTile -> CoordF MazeTile -> Bool
+isHorizontallyContinuous :: (KnownNat len, KnownNat wid) =>
+  CoordF len wid MazeTile -> CoordF len wid MazeTile -> Bool
 isHorizontallyContinuous a b =
   _y a == _y b && if
     | _x a == pred (_x b) ->
@@ -388,80 +271,25 @@ isHorizontallyContinuous a b =
       areHorizontallyContinuousTiles (_z b) (_z a)
     | otherwise -> False
 
-isHorizontallyContinuous' :: (KnownNat len, KnownNat wid) =>
-  CoordF' len wid MazeTile -> CoordF' len wid MazeTile -> Bool
-isHorizontallyContinuous' a b =
-  _y' a == _y' b && if
-    | _x' a == pred (_x' b) ->
-      areHorizontallyContinuousTiles (_z' a) (_z' b)
-    | _x' b == pred (_x' a) ->
-      areHorizontallyContinuousTiles (_z' b) (_z' a)
-    | otherwise -> False
-
-isVerticallyContinuous :: CoordF MazeTile -> CoordF MazeTile -> Bool
-isVerticallyContinuous a b =
-  _x a == _x b && if
-    | _y a == pred (_y b) ->
-      turn (_z a) North /= Stop && turn (_z b) South /= Stop
-    | _y b == pred (_y a) ->
-      turn (_z b) North /= Stop && turn (_z a) South /= Stop
-    | otherwise -> False
-
-isVerticallyContinuous' :: (KnownNat len, KnownNat wid) =>
-  CoordF' len wid MazeTile -> CoordF' len wid MazeTile -> Bool
-isVerticallyContinuous' a b =
-  _x' a == _x' b && if
-    | _y' a == pred (_y' b) ->
-      turn (_z' a) North /= Stop && turn (_z' b) South /= Stop
-    | _y' b == pred (_y' a) ->
-      turn (_z' b) North /= Stop && turn (_z' a) South /= Stop
-    | otherwise -> False
-
-isContinuous :: CoordF MazeTile -> CoordF MazeTile -> Bool
-isContinuous a b =
-  isHorizontallyContinuous a b && isVerticallyContinuous a b
-
-isContinuous' :: (KnownNat len, KnownNat wid) =>
-  CoordF' len wid MazeTile -> CoordF' len wid MazeTile -> Bool
-isContinuous' a b =
-  isHorizontallyContinuous' a b && isVerticallyContinuous' a b
-
-partitionLatitudes :: [CoordF a] -> [[CoordF a]]
+partitionLatitudes :: (KnownNat len, KnownNat wid) =>
+  [CoordF len wid a] -> [[CoordF len wid a]]
 partitionLatitudes =
   groupBy (on (==) _y) . sortBy (getComparison (
     contramap _y defaultComparison <> contramap _x defaultComparison))
 
-partitionLatitudes' :: (KnownNat len, KnownNat wid) =>
-  [CoordF' len wid a] -> [[CoordF' len wid a]]
-partitionLatitudes' =
-  groupBy (on (==) _y') . sortBy (getComparison (
-    contramap _y' defaultComparison <> contramap _x' defaultComparison))
-
-groupLatitude :: [CoordF MazeTile] -> [[CoordF MazeTile]]
+groupLatitude :: (KnownNat len, KnownNat wid) =>
+  [CoordF len wid MazeTile] -> [[CoordF len wid MazeTile]]
 groupLatitude = foldr (\a as -> case Data.List.uncons as of
   Nothing -> [[a]]
   Just (b, bs) -> if isHorizontallyContinuous a (head b)
     then (a:b):bs
     else [a]:as) []
 
-groupLatitude' :: (KnownNat len, KnownNat wid) =>
-  [CoordF' len wid MazeTile] -> [[CoordF' len wid MazeTile]]
-groupLatitude' = foldr (\a as -> case Data.List.uncons as of
-  Nothing -> [[a]]
-  Just (b, bs) -> if isHorizontallyContinuous' a (head b)
-    then (a:b):bs
-    else [a]:as) []
-
-isTangent :: [CoordF MazeTile] -> Bool
+isTangent :: (KnownNat len, KnownNat wid) =>
+  [CoordF len wid MazeTile] -> Bool
 isTangent [] = False
 isTangent [_] = False
 isTangent l = turn (_z . head $ l) West == turn (_z . last $ l) East
-
-isTangent' :: (KnownNat len, KnownNat wid) =>
-  [CoordF' len wid MazeTile] -> Bool
-isTangent' [] = False
-isTangent' [_] = False
-isTangent' l = turn (_z' . head $ l) West == turn (_z' . last $ l) East
 
 -- List hylomorphism
 hylo' :: (Maybe (c, b) -> b) -> (a -> Maybe (c, a)) -> a -> b
@@ -488,58 +316,32 @@ dropWhile1 p = para' $ \case
 takeDropWhile1 :: (a -> Bool) -> [a] -> ([a], [a])
 takeDropWhile1 p = takeWhile1 p &&& dropWhile1 p
 
-latitudeArea :: [[CoordF MazeTile]] -> Word
+latitudeArea :: (KnownNat len, KnownNat wid) =>
+  [[CoordF len wid MazeTile]] -> Word
 latitudeArea = hylo' alg coalg
   where
-    alg :: Maybe ([[CoordF MazeTile]], Word) -> Word
+    alg :: (KnownNat len, KnownNat wid) =>
+      Maybe ([[CoordF len wid MazeTile]], Word) -> Word
     alg = \case
       Nothing -> 0
       Just (segments, totalLength) -> gapLengths segments + totalLength
     gapLengths = sum <<<
       zipWith (\a b -> (_x . head $ b) - (_x . last $ a) - 1) <*> tail
-    coalg :: [[CoordF MazeTile]] -> Maybe ([[CoordF MazeTile]], [[CoordF MazeTile]])
+    coalg :: (KnownNat len, KnownNat wid) =>
+      [[CoordF len wid MazeTile]] ->
+        Maybe ([[CoordF len wid MazeTile]], [[CoordF len wid MazeTile]])
     coalg = fmap (uncurry (first . (:)) . second (takeDropWhile1 isTangent))
       . uncons . dropWhile isTangent
 
-latitudeArea' :: (KnownNat len, KnownNat wid) =>
-  [[CoordF' len wid MazeTile]] -> Word
-latitudeArea' = hylo' alg coalg
-  where
-    alg :: (KnownNat len, KnownNat wid) =>
-      Maybe ([[CoordF' len wid MazeTile]], Word) -> Word
-    alg = \case
-      Nothing -> 0
-      Just (segments, totalLength) -> gapLengths segments + totalLength
-    gapLengths = sum <<<
-      zipWith (\a b -> (_x' . head $ b) - (_x' . last $ a) - 1) <*> tail
-    coalg :: (KnownNat len, KnownNat wid) =>
-      [[CoordF' len wid MazeTile]] ->
-        Maybe ([[CoordF' len wid MazeTile]], [[CoordF' len wid MazeTile]])
-    coalg = fmap (uncurry (first . (:)) . second (takeDropWhile1 isTangent'))
-      . uncons . dropWhile isTangent'
-
-coordWithTile :: Maze -> CoordF a -> CoordF MazeTile
+coordWithTile :: (KnownNat len, KnownNat wid) =>
+  Maze len wid -> CoordF len wid a -> CoordF len wid MazeTile
 coordWithTile maze = ($>) <*> indexAdjunction maze
 
-coordWithTile' :: (KnownNat len, KnownNat wid) =>
-  Maze' len wid -> CoordF' len wid a -> CoordF' len wid MazeTile
-coordWithTile' maze = ($>) <*> indexAdjunction maze
-
-pathWithTiles :: Maze -> Step -> [CoordF MazeTile]
+pathWithTiles :: (KnownNat len, KnownNat wid) =>
+  Maze len wid -> Step len wid -> [CoordF len wid MazeTile]
 pathWithTiles maze = fmap (coordWithTile maze) . mazePath maze
 
-pathWithTiles' :: (KnownNat len, KnownNat wid) =>
-  Maze' len wid -> Step' len wid -> [CoordF' len wid MazeTile]
-pathWithTiles' maze = fmap (coordWithTile' maze) . mazePath' maze
-
 partB :: Input -> OutputB
-partB = auf (_Wrapping Sum) (foldMapOf traverse) (latitudeArea . groupLatitude)
-  . partitionLatitudes . uncurry pathWithTiles
-
-partB' :: Input' -> OutputB
-partB' (len, wid, mazeStart) = withSomeSNat len $ \case
-  SNat @len -> withSomeSNat wid $ \case
-    SNat @wid -> case mazeStart of
-      MazeStart maze start ->
-        auf (_Wrapping Sum) (foldMapOf traverse) (latitudeArea' . groupLatitude')
-          . partitionLatitudes' $ pathWithTiles' maze start
+partB MazeStart { maze, start } =
+  auf (_Wrapping Sum) (foldMapOf traverse) (latitudeArea . groupLatitude)
+    . partitionLatitudes $ pathWithTiles maze start
