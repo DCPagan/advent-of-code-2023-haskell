344a8dfad8bc25bf1f0ca6196055e8d7e6dca810
D.C. Pagan <dcpagan@posteo.net>
2024-10-06 02:52:33 -0400

    Day 13, part 2

--- src/Days/Day13.hs
+++ src/Days/Day13.hs
@@ -1,15 +1,20 @@
 {-# LANGUAGE GADTs #-}
+
+{-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Days.Day13 (runDay) where
 
-import Control.Applicative
 import Control.Arrow
-import Control.Lens hiding (cons,snoc,uncons,unsnoc)
+import Control.Lens hiding (cons,uncons)
 import Control.Monad
+import Control.Monad.Combinators
+import Control.Monad.ST
 
 import Data.Array.IArray
-import Data.Attoparsec.Text hiding (take)
+import Data.Array.MArray
+import Data.Array.ST
+import Data.Attoparsec.Text (Parser,char,endOfLine)
 import Data.Coerce
 import Data.Function
 import Data.Functor
@@ -17,10 +22,10 @@ import Data.Ix
 import Data.List
 import Data.Maybe
 import Data.Monoid
-import Data.Void
 
 import qualified Program.RunDay as R (Day,runDay)
 
+import qualified Text.ParserCombinators.ReadP as R
 import qualified Text.ParserCombinators.ReadPrec as RP
 import Text.Read
 
@@ -28,9 +33,10 @@ import qualified Util.Util as U
 
 ------------ TYPES ------------
 data Ground where
-  Ash :: Ground   -- .
-  Rock :: Ground  -- #
-  Error :: Ground -- ¿
+  Ash :: Ground    -- .
+  Rock :: Ground   -- #
+  Smudge :: Ground -- $
+  Error :: Ground  -- ¿
   deriving (Bounded,Enum,Eq,Ord)
 
 makePrisms ''Ground
@@ -48,24 +54,48 @@ instance Show Ground where
 toGround :: Char -> Ground
 toGround '.' = Ash
 toGround '#' = Rock
+toGround '$' = Rock
 toGround _ = Error
 
 fromGround :: Ground -> Char
 fromGround Ash = '.'
 fromGround Rock = '#'
+fromGround Smudge = '$'
 fromGround Error = '¿'
 
-type Grid = Array (Word, Word) Ground
+newtype Grid = Grid
+  { unGrid :: Array (Word, Word) Ground
+  }
+  deriving (Eq,Ord)
+
+instance Read Grid where
+  readPrec = toGrid <$> some row
+    where
+      cell = toGround <$> RP.get
+
+      row = some cell <* endOfLine
+
+      endOfLine = lift $ R.char '\n' $> () <|> R.string "\r\n" $> ()
+
+  readListPrec = sepBy readPrec endOfLine
+    where
+      endOfLine = lift $ R.char '\n' $> () <|> R.string "\r\n" $> ()
+
+instance Show Grid where
+  show = intercalate "\n" . fmap (show . fmap snd) . groupBy
+    (on (==) $ fst . fst) . assocs . unGrid
+
+  showList ls s = intercalate "\n\n" (fmap show ls) ++ s
 
 type Input = [Grid]
 
 type OutputA = Word
 
-type OutputB = Void
+type OutputB = Word
 
 ------------ PARSER ------------
 toGrid :: [[Ground]] -> Grid
-toGrid g = array bounds $ zip (range bounds) $ join g
+toGrid g = Grid $ array bounds $ zip (range bounds) $ join g
   where
     bounds = ((0, 0), on (,) (fromIntegral . pred . length) <*> transpose $ g)
 
@@ -92,25 +122,25 @@ inputParser = grids
 
 ------------ PART A ------------
 northReflectionIndices :: Grid -> Word -> [(Word, Word)]
-northReflectionIndices g y' = do
+northReflectionIndices (Grid g) y' = do
   y <- reverse . take (fromIntegral y') . range <<< each %~ fst $ bounds g
   x <- range <<< each %~ snd $ bounds g
   return (y, x)
 
 southReflectionIndices :: Grid -> Word -> [(Word, Word)]
-southReflectionIndices g y' = do
+southReflectionIndices (Grid g) y' = do
   y <- drop (fromIntegral y') . range <<< each %~ fst $ bounds g
   x <- range <<< each %~ snd $ bounds g
   return (y, x)
 
 westReflectionIndices :: Grid -> Word -> [(Word, Word)]
-westReflectionIndices g x' = do
+westReflectionIndices (Grid g) x' = do
   x <- reverse . take (fromIntegral x') . range <<< each %~ snd $ bounds g
   y <- range <<< each %~ fst $ bounds g
   return (y, x)
 
 eastReflectionIndices :: Grid -> Word -> [(Word, Word)]
-eastReflectionIndices g x' = do
+eastReflectionIndices (Grid g) x' = do
   x <- drop (fromIntegral x') . range <<< each %~ snd $ bounds g
   y <- range <<< each %~ fst $ bounds g
   return (y, x)
@@ -124,18 +154,18 @@ reflectLatitudeIndices g x =
   zip (northReflectionIndices g x) (southReflectionIndices g x)
 
 isSymmetricOnLongitude :: Grid -> Word -> Bool
-isSymmetricOnLongitude g = all (uncurry (on (==) (g !)))
+isSymmetricOnLongitude g = all (uncurry (on (==) (unGrid g !)))
   . reflectLongitudeIndices g
 
 isSymmetricOnLatitude :: Grid -> Word -> Bool
-isSymmetricOnLatitude g = all (uncurry (on (==) (g !)))
+isSymmetricOnLatitude g = all (uncurry (on (==) (unGrid g !)))
   . reflectLatitudeIndices g
 
 getLongitudes :: Grid -> [Word]
-getLongitudes = range <<< _1 %~ succ <<< each %~ snd <<< bounds
+getLongitudes = range <<< _1 %~ (1 +) <<< each %~ snd <<< bounds . unGrid
 
 getLatitudes :: Grid -> [Word]
-getLatitudes = range <<< _1 %~ succ <<< each %~ fst <<< bounds
+getLatitudes = range <<< _1 %~ (1 +) <<< each %~ fst <<< bounds . unGrid
 
 longitudeSymmetryScore :: Grid -> Word
 longitudeSymmetryScore g = sum . filter (isSymmetricOnLongitude g)
@@ -152,8 +182,53 @@ partA :: Input -> OutputA
 partA = getSum . foldMap (coerce . symmetryScore)
 
 ------------ PART B ------------
+assymetriesOnLongitude :: Grid -> Word -> [((Word, Word), (Word, Word))]
+assymetriesOnLongitude g = filter (uncurry (on (/=) (unGrid g !)))
+  . reflectLongitudeIndices g
+
+assymetriesOnLatitude :: Grid -> Word -> [((Word, Word), (Word, Word))]
+assymetriesOnLatitude g = filter (uncurry (on (/=) (unGrid g !)))
+  . reflectLatitudeIndices g
+
+getLongitudeSmudges :: Grid -> [((Word, Word), (Word, Word))]
+getLongitudeSmudges g = do
+  longAssymetries <- assymetriesOnLongitude g <$> getLongitudes g
+  guard (length longAssymetries == 1) *> longAssymetries
+
+getLatitudeSmudges :: Grid -> [((Word, Word), (Word, Word))]
+getLatitudeSmudges g = do
+  latAssymetries <- assymetriesOnLatitude g <$> getLatitudes g
+  guard (length latAssymetries == 1) *> latAssymetries
+
+getSmudges :: Grid -> [((Word, Word), (Word, Word))]
+getSmudges g = nub $ getLongitudeSmudges g <|> getLatitudeSmudges g
+
+flipGround :: Ground -> Ground
+flipGround Ash = Rock
+flipGround Rock = Ash
+flipGround x = x
+
+cleanSmudges :: Grid -> Grid
+cleanSmudges g' = Grid $ runSTArray $ do
+  let smudges = getSmudges g'
+  g <- thaw $ unGrid g'
+  mapM_ (flip (modifyArray g) flipGround . fst) smudges
+  return g
+
+getLongitudesWithSmudge :: Grid -> [Word]
+getLongitudesWithSmudge g = filter
+  (\l -> length (assymetriesOnLongitude g l) == 1) . getLongitudes $ g
+
+getLatitudesWithSmudge :: Grid -> [Word]
+getLatitudesWithSmudge g = filter
+  (\l -> length (assymetriesOnLatitude g l) == 1) . getLatitudes $ g
+
+symmetryScoreWithSmudges :: Grid -> Word
+symmetryScoreWithSmudges g = sum (getLongitudesWithSmudge g)
+  + 100 * sum (getLatitudesWithSmudge g)
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB = getSum . foldMap (coerce . symmetryScoreWithSmudges)
 
 runDay :: R.Day
 runDay = R.runDay inputParser partA partB
